d67a216d96e721cb2cf10088e0aaaac4ab56b73d
1lymax <59887205+1lymax@users.noreply.github.com>
2023-01-04 20:53:57 +0200

    minor fix

--- store/slices/playerSlice.ts
+++ store/slices/playerSlice.ts
@@ -37,7 +37,6 @@ export const playerSlice = createSlice({
     },
     extraReducers: {
         [HYDRATE]: (state, action) => {
-            //console.log('HYDRATE', state, action.payload);
             return {
                 ...state,
                 ...action.payload.player,
