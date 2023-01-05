908d1ee3d196d9d9689ae3f4af8840337af3a48d
1lymax <59887205+1lymax@users.noreply.github.com>
2023-01-05 16:25:57 +0200

    minor

--- pages/tracks/index.tsx
+++ pages/tracks/index.tsx
@@ -1,9 +1,9 @@
 import React, {FC} from 'react';
-import MainLayout from "../../layouts/MainLayout";
 import styled from "styled-components";
 import {Button} from "@mui/material";
 import {useRouter} from "next/router";
 import {ITrack} from "../../types/track";
+import MainLayout from "../../layouts/MainLayout";
 import TrackList from "../../components/TrackList";
 import {getAllTracks, trackApi} from "../../store/api/track";
 import {wrapper} from "../../store";
