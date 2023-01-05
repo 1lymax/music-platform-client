009542768fb60040355c510972d02da76d54097e
1lymax <59887205+1lymax@users.noreply.github.com>
2023-01-05 10:20:15 +0200

    hooks update

---
+++ hooks/useAlbumActions.ts
@@ -0,0 +1,8 @@
+import {useDispatch} from "react-redux";
+import {bindActionCreators} from "redux";
+import {albumActionCreators} from "../store/slices/albumSlice";
+
+export const useAlbumActions = () => {
+    const dispatch = useDispatch()
+    return bindActionCreators(albumActionCreators, dispatch)
+}
