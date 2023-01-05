fac571e375b96595a27f66da5b48f049060c1184
1lymax <59887205+1lymax@users.noreply.github.com>
2023-01-05 16:25:28 +0200

    selectbox

--- components/SelectBox.tsx
+++ components/SelectBox.tsx
@@ -1,28 +1,25 @@
 import {FC} from 'react';
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
+import {IArtist} from "../types/artist";
+import {IAlbum} from "../types/album";
 
 interface SelectBoxProps {
-    options: {
-        _id: string;
-        name: string
-    }[];
+    options: IArtist[] | IAlbum[];
     setValue: (value:any) => void;
     label: string
 }
 
 
-const SelectBox: FC<SelectBoxProps>= ({options, setValue, label}) => {
-
-
+const SelectBox: FC<SelectBoxProps> = ({options, setValue, label}) => {
     return (
         <Autocomplete
-            disablePortal
-            id="combo-box-demo"
+            sx={{width: '100%'}}
             options={options}
             clearOnEscape
-            onChange={(value:any) => setValue(value._id)}
+            onChange={(event, value:any) => setValue(value)}
             getOptionLabel={(option) => option.name}
+            isOptionEqualToValue={(option, value)=> option._id === value._id}
             renderInput={(params) => <TextField {...params} label={label}/>}
         />
     )
